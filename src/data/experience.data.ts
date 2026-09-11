58e998320fbcf96e5bc71fdc4c057e3c0abedea9
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2026-09-11 13:18:48 +0800

    Revise experience and academic data entries
    Updated work experiences and academic background with new roles and details.

--- src/data/experience.data.ts
+++ src/data/experience.data.ts
@@ -1,59 +1,59 @@
-import type { ExperienceItem, Certification } from "@/types"
+import type { ExperienceItem, Certification } from "@/types";
 
 export const workExperiences: ExperienceItem[] = [
   {
-    title:        "Backend Web Developer",
-    organization: "Progressive Medical Corporation",
-    date:         "Aug 2025 - Present",
-    address:      "200 C. Raymundo Avenue Caniogan Pasig City",
-    url:          "https://pmc.ph/",
-  },
-  {
-    title:        "Web Developer Intern",
-    organization: "Wizzard Technologies, Inc.",
-    date:         "Mar 2025 - Jun 2025",
-    address:      "Unit 204 The Centerpoint Building Julia Vargas Ave. Ortigas Pasig City",
-    url:          "https://www.facebook.com/wtiwizzard/",
-  },
-  {
-    title:        "Freelance Developer",
-    organization: "Self Employed",
-    date:         "Jul 2023 - May 2024",
-    address:      "Remote",
-    url:          "https://github.com/ShepherdBoyy",
-  },
-  {
-    title:        "Branch Administrator",
+    title: "Branch Administrator",
     organization: "JnT Express",
-    date:         "Sep 2023 - Dec 2024",
-    address:      "Pinagbuhatan Pasig City",
-    url:          "https://www.jtexpress.ph/",
+    date: "Sep 2023 - Dec 2024",
+    address: "Pinagbuhatan Pasig City",
+    url: "https://www.jtexpress.ph/",
   },
-]
+  {
+    title: "Freelance Developer",
+    organization: "Self Employed",
+    date: "Jul 2023 - May 2024",
+    address: "Remote",
+    url: "https://github.com/ShepherdBoyy",
+  },
+  {
+    title: "Web Developer Intern",
+    organization: "Wizzard Technologies, Inc.",
+    date: "Mar 2025 - Jun 2025",
+    address: "Unit 204 The Centerpoint Building Julia Vargas Ave. Ortigas Pasig City",
+    url: "https://www.facebook.com/wtiwizzard/",
+  },
+  {
+    title: "Backend Web Developer",
+    organization: "Progressive Medical Corporation",
+    date: "Aug 2025 - Present",
+    address: "200 C. Raymundo Avenue Caniogan Pasig City",
+    url: "https://pmc.ph/",
+  },
+];
 
 export const academicBackground: ExperienceItem[] = [
   {
-    title:        "Bachelor of Science in Computer Science",
-    organization: "EULOGIO 'AMANG' RODRIGUEZ INSTITUTE OF SCIENCE AND TECHNOLOGY",
-    date:         "2021 - 2025",
-    address:      "Nagtahan St, Sampaloc, Manila, 1008 Metro Manila",
-    url:          "https://earist.edu.ph/",
-  },
-  {
-    title:        "Accountancy, Business, and Management",
-    organization: "Rizal Technological University",
-    date:         "2019 - 2021",
-    address:      "Eusebio Avenue, Barangay Maybunga, Pasig City, Metro Manila",
-    url:          "https://www.rtu.edu.ph/",
-  },
-  {
-    title:        "Junior High School",
+    title: "Junior High School",
     organization: "Nagpayong High School",
-    date:         "2015 - 2019",
-    address:      "Centennial II, Nagpayong, Pinagbuhatan, Pasig City",
-    url:          "https://www.facebook.com/p/Nagpayong-High-School-Pasig-100063991754976/",
+    date: "2015 - 2019",
+    address: "Centennial II, Nagpayong, Pinagbuhatan, Pasig City",
+    url: "https://www.facebook.com/p/Nagpayong-High-School-Pasig-100063991754976/",
   },
-]
+  {
+    title: "Accountancy, Business, and Management",
+    organization: "Rizal Technological University",
+    date: "2019 - 2021",
+    address: "Eusebio Avenue, Barangay Maybunga, Pasig City, Metro Manila",
+    url: "https://www.rtu.edu.ph/",
+  },
+  {
+    title: "Bachelor of Science in Computer Science",
+    organization: "EULOGIO 'AMANG' RODRIGUEZ INSTITUTE OF SCIENCE AND TECHNOLOGY",
+    date: "2021 - 2025",
+    address: "Nagtahan St, Sampaloc, Manila, 1008 Metro Manila",
+    url: "https://earist.edu.ph/",
+  },
+];
 
 export const certifications: Certification[] = [
   {
@@ -61,35 +61,34 @@ export const certifications: Certification[] = [
     issuer: "freeCodeCamp",
     issuedDate: "September 2024",
     image: "/certs/back-end-development-and-apis.png",
-    pdfUrl: "/certs/back-end-development-and-apis.pdf"
-
+    pdfUrl: "/certs/back-end-development-and-apis.pdf",
   },
   {
     name: "Relational Database",
     issuer: "freeCodeCamp",
     issuedDate: "August 2024",
     image: "/certs/relational-database.png",
-    pdfUrl: "/certs/relational-database.pdf"
+    pdfUrl: "/certs/relational-database.pdf",
   },
   {
     name: "Front-End Development Libraries",
     issuer: "freeCodeCamp",
     issuedDate: "June 2024",
     image: "/certs/front-end-development-libraries.png",
-    pdfUrl: "/certs/front-end-development-libraries.pdf"
+    pdfUrl: "/certs/front-end-development-libraries.pdf",
   },
   {
     name: "Legacy JavaScript Algorithms and Data Structures",
     issuer: "freeCodeCamp",
     issuedDate: "April 2023",
     image: "/certs/legacy-javascript-algorithms-and-data-structures.png",
-    pdfUrl: "/certs/legacy-javascript-algorithms-and-data-structures.pdf"
+    pdfUrl: "/certs/legacy-javascript-algorithms-and-data-structures.pdf",
   },
   {
     name: "Legacy Responsive Web Design",
     issuer: "freeCodeCamp",
     issuedDate: "February 2023",
     image: "/certs/legacy-responsive-web-design.png",
-    pdfUrl: "/certs/legacy-responsive-web-design.pdf"
+    pdfUrl: "/certs/legacy-responsive-web-design.pdf",
   },
-]
+];
